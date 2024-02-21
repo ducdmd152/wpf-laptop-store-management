18e7ddc73cccbc8526b49209fb1463fa0d78372b
Duc Dao <90080923+ducdmd152@users.noreply.github.com>
2024-02-21 08:59:32 +0700

    Upload source code to Git Repo

---
+++ MyShopManagementRepository/UserRepository.cs
@@ -0,0 +1,21 @@
+﻿using MyShopManagementBO;
+using MyShopManagementDAO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopManagementRepository
+{
+    public class UserRepository : IUserRepository
+    {
+        public List<User> GetAll() => UserDAO.Instance.GetAll();
+        public List<Role> GetRoles() => UserDAO.Instance.GetRoles();
+        public User Get(string email) => UserDAO.Instance.Get(email);
+        public void Create(User entity) => UserDAO.Instance.Create(entity);
+        public void Update(User entity) => UserDAO.Instance.Update(entity);
+        public void Delete(string email) => UserDAO.Instance.Delete(email);
+        public bool Exist(string email) => UserDAO.Instance.Exist(email);
+    }
+}
---
+++ MyShopManagementService/UserService.cs
@@ -0,0 +1,26 @@
+﻿using MyShopManagementBO;
+using MyShopManagementRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShopManagementService
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+        public UserService() {
+            _userRepository = new UserRepository();
+        }
+
+        public List<User> GetAll() => _userRepository.GetAll();
+        public List<Role> GetRoles() => _userRepository.GetRoles();
+        public User Get(string email) => _userRepository.Get(email);
+        public void Create(User entity) => _userRepository.Create(entity);
+        public void Update(User entity) => _userRepository.Update(entity);
+        public void Delete(string email) => _userRepository.Delete(email);
+        public bool Exist(string email) => _userRepository.Exist(email);
+    }
+}
